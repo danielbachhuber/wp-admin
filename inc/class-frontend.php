f50f2c30e273e6712dac2e63f29d585d4e9c909d
Daniel Bachhuber <d@danielbachhuber.com>
2014-12-07 21:16:39 -0800

    Fix foundation

--- inc/class-frontend.php
+++ inc/class-frontend.php
@@ -37,7 +37,7 @@ class Frontend {
 	 */
 	public function action_wp_enqueue_scripts() {
 
-		wp_enqueue_style( 'wp-admin', get_template_directory_uri() . '/assets/css/theme.css' );
+		wp_enqueue_style( 'wp-admin-theme', get_template_directory_uri() . '/assets/css/theme.css' );
 
 	}
 
